b3b2a11512b39a25ff8f7bab221137cb5da2e25d
Luzze <luzzekizza@gmail.com>
2026-06-14 19:58:59 +0300

    Initial React + TS + Vite routing structure

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,10 @@
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <h1>UFE League OS</h1>
+      <p>Welcome to the Unified Fan Engagement & League Platform</p>
+    </div>
+  );
+}
+
+export default LandingPage;
