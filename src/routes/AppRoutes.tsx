fe9bf4f1e1db5ce8d115c2089e74e7e9041eaa7a
merab <merab.apio@gmail.com>
2026-06-29 22:27:19 +0300

    Update AppRoutesin ranking

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -68,12 +68,19 @@ import MyTicketsPage from '../pages/fan/MyTicketsPage';
 import TicketDetailPage from '../pages/fan/TicketDetailPage';
 import ProtectedRoute from './ProtectedRoute';
 import Payments from '../pages/PaymentPage';
-import Rankings from '../pages/Rankings';
+<<<<<<< HEAD
+
+=======
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> b059106 (Update AppRoutes)
 import FanPollsPage from '../pages/fan/FanPollsPage'; // ← new
 import TicketCheckoutPage from '../pages/tickets/TicketCheckoutPage';
 import TicketPaymentProcessingPage from '../pages/tickets/TicketPaymentProcessingPage';
 import TicketPaymentSuccessPage from '../pages/tickets/TicketPaymentSuccessPage';
 import TicketPaymentFailedPage from '../pages/tickets/TicketPaymentFailedPage';
+
 
 
 function protectedPage(page: ReactNode) {
@@ -158,7 +165,7 @@ export default function AppRoutes() {
                 <Route path="/results" element={<Results />} />
                 <Route path="/support" element={<Support />} />
                 <Route path="/about" element={<AboutUs />} />
-                <Route path="/rankings" element={<Rankings />} />
+
 
                 <Route path="/payments" element={<Payments />} />
             </Routes>
