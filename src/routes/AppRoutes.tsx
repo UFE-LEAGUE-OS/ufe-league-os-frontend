1d354d37a272b6c941f5ac68fbdeab0781e67f40
@Babirye366 <babiryemable442@gmail.com>
2026-07-06 15:21:48 +0300

    extra work

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -84,6 +84,9 @@ import FantasyModuleConfig from "../pages/super-admin/fantasy/FantasyModuleConfi
 import ScoringRules from "../pages/super-admin/fantasy/ScoringRules";
 import TransferRules from "../pages/super-admin/fantasy/TransferRules";
 import SquadLimits from "../pages/super-admin/fantasy/SquadLimits";
+import CompetitionMappings from "../pages/super-admin/fantasy/CompetitionMappings";
+import SeasonGameweekSettings from "../pages/super-admin/fantasy/SeasonGameweekSettings";
+import PriceStructureGovernance from "../pages/super-admin/fantasy/PriceStructureGovernance";
 import SuperAdminHome from '../pages/super-admin/SuperAdminHome';
 import UserManagement    from '../pages/admin/UserManagement';
 import RoleTemplates     from '../pages/admin/RoleTemplates';
@@ -250,6 +253,9 @@ export default function AppRoutes() {
                     <Route path="fantasy-config/scoring" element={<ScoringRules />} />
                     <Route path="fantasy-config/transfers" element={<TransferRules />} />
                     <Route path="fantasy-config/squad-limits" element={<SquadLimits />} />
+                    <Route path="fantasy-config/competition-mappings" element={<CompetitionMappings />} />
+                    <Route path="fantasy-config/season-gameweek" element={<SeasonGameweekSettings />} />
+                    <Route path="fantasy-config/price-structure" element={<PriceStructureGovernance />} />
                 </Route>
             </Routes>
 
